55afef9df75a4b7e02acd141a64d923c0ab7e9b8
JB <josh.bissessar@gmail.com>
2021-03-07 23:11:39 -0500

    Adding challenge files

---
+++ Mission_to_Mars_Challenge.py
@@ -0,0 +1,189 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# Import Splinter, BeautifulSoup, and Pandas
+from splinter import Browser
+from bs4 import BeautifulSoup as soup
+import pandas as pd
+
+
+# In[2]:
+
+
+# Set the executable path and initialize the chrome browser in splinter
+executable_path = {'executable_path': 'chromedriver'}
+browser = Browser('chrome', **executable_path)
+
+
+# ### Visit the NASA Mars News Site
+
+# In[3]:
+
+
+# Visit the mars nasa news site
+url = 'https://mars.nasa.gov/news/'
+browser.visit(url)
+
+# Optional delay for loading the page
+browser.is_element_present_by_css("ul.item_list li.slide", wait_time=1)
+
+
+# In[4]:
+
+
+# Convert the browser html to a soup object and then quit the browser
+html = browser.html
+news_soup = soup(html, 'html.parser')
+
+slide_elem = news_soup.select_one('ul.item_list li.slide')
+
+
+# In[5]:
+
+
+slide_elem.find("div", class_='content_title')
+
+
+# In[6]:
+
+
+# Use the parent element to find the first a tag and save it as `news_title`
+news_title = slide_elem.find("div", class_='content_title').get_text()
+news_title
+
+
+# In[7]:
+
+
+# Use the parent element to find the paragraph text
+news_p = slide_elem.find('div', class_="article_teaser_body").get_text()
+news_p
+
+
+# ### JPL Space Images Featured Image
+
+# In[8]:
+
+
+# Visit URL
+url = 'https://data-class-jpl-space.s3.amazonaws.com/JPL_Space/index.html'
+browser.visit(url)
+
+
+# In[9]:
+
+
+# Find and click the full image button
+full_image_elem = browser.find_by_tag('button')[1]
+full_image_elem.click()
+
+
+# In[10]:
+
+
+# Parse the resulting html with soup
+html = browser.html
+img_soup = soup(html, 'html.parser')
+
+
+# In[11]:
+
+
+# Find the relative image url
+img_url_rel = img_soup.find('img', class_='fancybox-image').get('src')
+img_url_rel
+
+
+# In[12]:
+
+
+# Use the base url to create an absolute url
+img_url = f'https://data-class-jpl-space.s3.amazonaws.com/JPL_Space/{img_url_rel}'
+img_url
+
+
+# ### Mars Facts
+
+# In[13]:
+
+
+# Create a new DataFrame from the HTML table
+df = pd.read_html('https://space-facts.com/mars/')[0]
+
+
+# In[14]:
+
+
+# Assign columns & index to the new DF
+df.columns=['Description', 'Mars']
+df.set_index('Description', inplace=True)
+df
+
+
+# In[15]:
+
+
+df.to_html()
+
+
+# # D1: Scrape High-Resolution Mars’ Hemisphere Images and Titles
+
+# ### Hemispheres
+
+# In[16]:
+
+
+# 1. Use browser to visit the URL 
+url = 'https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
+browser.visit(url)
+
+
+# In[17]:
+
+
+# 2. Create a list to hold the images and titles.
+hemisphere_image_urls = []
+
+# 3. Write code to retrieve the image urls and titles for each hemisphere.
+for x in range (4):
+    
+    # Navigate to the hemisphere details
+    browser.find_by_css('h3')[x].click()
+    
+    # Create HTML object & parse with 'html.parser'
+    html = browser.html
+    hemi_soup = soup(html, 'html.parser')
+    
+    # Scrape URL for full resolution image
+    img_url = hemi_soup.find('a', text="Sample").get('href')
+
+    # Scrape hemisphere title 
+    title = hemi_soup.find('h2', class_='title').get_text()
+    
+    # Add dictionary items to list
+    hemisphere_image_urls.append({'img_url': img_url, 
+                                  'title': title})
+    
+    # Navigate back to previous page
+    browser.back()
+    
+    print(img_url)
+    print(title)
+
+
+# In[18]:
+
+
+# 4. Print the list that holds the dictionary of each image url and title.
+hemisphere_image_urls
+
+
+# In[19]:
+
+
+# 5. Quit the browser
+browser.quit()
+
--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # Mission-to-Mars
+
+## Project Overview
+
+The Mission to Mars WebApp was developed to scrape data from various websites using BeautifulSoup and Splinter to provide the user with the following data on the Planet Mars:
+
+- Latest news regarding Mars provided by NASA.
+- A featured Image of Mars provided by JPL at the California Institute of Technology
+- An array of facts about the planet.
+- Full-resolution images and titles of the four hemispheres on Mars.
+
+
+## Resources 
+
+- Software: Anaconda 4.9.2, Jupyter Notebook 6.1.4, Python 3.8.5, Visual Studio Code 1.54.1
+
+- Data Files: 
+  - [Mission_to_Mars_Challenge]()
+  - [scraping.py]()
+  - [app.py]()
+  - [index.html]()
